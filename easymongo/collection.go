4fb3affd440d60ab02564f2c334f80303ce40c00
Topher Sterling <topher@develops.guru>
2020-11-17 22:53:31 -0700

    Fix vet.

--- easymongo/collection.go
+++ easymongo/collection.go
@@ -180,7 +180,9 @@ func (c *Collection) DeleteOne()                {}
 func (c *Collection) DeleteMany()               {}
 func (c *Collection) DeleteByID(id interface{}) {}
 
-func (c *Collection) Insert() *InsertQuery {}
+func (c *Collection) Insert() *InsertQuery {
+	return &InsertQuery{}
+}
 
 // func (c *Collection) InsertMany() {}
 
